e93f075ee5349747061f4e95c2bcd68760afaec0
Rafaelfcs <rafael.ferigolo@gmail.com>
2020-04-08 17:01:34 -0300

    Superlogica API - Contrato.all

--- lib/base.rb
+++ lib/base.rb
@@ -13,4 +13,14 @@ class Base
     response = @middleware.do_request(api, query, http_method)
     response.body
   end
+
+  def self.all(func, http_method)
+    klass = to_s
+    api, object = get_api_and_caller_object_name(klass)
+    api = api.downcase
+    object = object.downcase
+    query = @query_builder.build(api, object, func)
+    response = @middleware.do_request(api, query, http_method)
+    response.body
+  end
 end
