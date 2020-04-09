35ed238cc9892749398c3c7e93c1f5c6d074c286
Caio Marques <marques@caiena.net>
2020-04-08 21:39:00 -0300

    Fix Zendesk::Ticket

--- lib/zendesk/ticket.rb
+++ lib/zendesk/ticket.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'base'
+require 'parser/zendesk_ticket_parser'
 
 # Zendesk module.
 module Zendesk
@@ -19,17 +20,6 @@ module Zendesk
 
     def self.find(id)
       response_body = super('GET', { id: id })
-    end
-
-    def self.parse(response, method)
-      response = JSON.parse(response)
-      method = "parse_#{method}"
-      Ticket.send(method, response)
-    end
-
-    def self.parse_find(response)
-      ticket_id = response['ticket']['id']
-      Ticket.new(ticket_id)
     end
   end
 end
